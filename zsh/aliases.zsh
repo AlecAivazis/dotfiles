497f2366208ce16412819c187c7fabbdff77cfb1
alec aivazis <alec@aivazis.com>
2015-03-29 11:53:16 -0700

    initial commit

---
+++ zsh/aliases.zsh
@@ -0,0 +1,5 @@
+# Colorize output, add file type indicator, and put sizes in human readable format
+alias ls='ls -GFh'
+
+# Same as above, but in long listing format
+alias ll='ls -GFhl'
---
+++ zsh/history.zsh
@@ -0,0 +1,6 @@
+# HISTORY
+HISTSIZE=3000
+SAVEHIST=3000
+HISTFILE=~/.zsh_history
+
+bindkey '^R' zaw-history
