4f5d7db13a6cdde2e10c7f74191e4e57e58cc98a
alec aivazis <alec@aivazis.com>
2015-03-31 19:41:53 -0700

    removed CDPATH cuz it was pissing me off

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,12 +1,14 @@
 # add the user specific bin to the path
 export PATH=~/bin:/opt/local/bin:${PATH}
-# add the dv directory to the cd path
-export CDPATH=~/dv:${CDPATH}
 
 # Setup terminal, and turn on colors
 export TERM=xterm-256color
 export CLICOLOR=1
 export LSCOLORS=Gxfxcxdxbxegedabagacad
+
+# default editor settings
+export EDITOR=/usr/bin/vim
+export VISUAL=/usr/bin/vim
 
 # Enable color in grep
 export GREP_OPTIONS='--color=auto'
