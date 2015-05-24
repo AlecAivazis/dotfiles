8207befdae3139f451f07ecf143250de6c203d4e
alec aivazis <alec@aivazis.com>
2015-05-23 17:04:52 -0700

    added EOL and BOL shortcuts

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,8 @@
 # Colorize output, add file type indicator, and put sizes in human readable format
 alias ll='ls -GAhl'
 
-# mm aliases
+## mm aliases
+
 alias mm='python3 ${HOME}/dv/config/make/mm.py'
 alias mm.env='mm --env=sh'
 alias mm.show='mm --show --dry'
@@ -17,3 +18,17 @@ mm.paths() {
     # and remove the temporary
     rm -f ${tmpfile}
 }
+
+## authorea aliases
+
+alias au-start-thin='thin start'
+alias au-start-redis='bundle-1.9 exec redis-server'
+alias au-start-foreman='bundle-1.9 exec foreman start'
+# start each of the services in their own tab
+alias au-start='tab au-start-thin ; tab au-start-redis ; tab au-start-foreman'
+
+alias au-kill-foreman='ps -ef |grep foreman|awk '"'"'NR==1{print $2}'"'"'|xargs kill'
+alias au-kill-faye='lsof -i:9292 |grep LISTEN|awk '"'"'NR==1{print $2}'"'"'|xargs kill -9'
+alias au-kill-redis='lsof -i:6379 |grep LISTEN|awk '"'"'NR==1{print $2}'"'"'|xargs kill'
+alias au-kill-thin='lsof -i:3000 |grep LISTEN|awk '"'"'NR==1{print $2}'"'"'|xargs kill'
+alias au-kill='au-kill-redis ; au-kill-foreman ; au-kill-thin ; au-kill-faye'
