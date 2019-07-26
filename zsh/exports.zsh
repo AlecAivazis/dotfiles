55adbcb1fd4a4647d83df4a8c45dfc7d6f0b24f2
Alec Aivazis <alec@aivazis.com>
2019-07-26 09:47:33 -0700

    fixed typo

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -41,4 +41,4 @@ fi
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" #
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
