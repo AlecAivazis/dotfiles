e18d0e0194c91392f66eea69a3dd36c7f76a7328
Alec Aivazis <alec@aivazis.com>
2025-03-10 00:18:15 -0700

    update style

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -1,25 +1,18 @@
 -- This file needs to have same structure as nvconfig.lua 
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
 -- Please read that file to know all available options :( 
-
+--
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
 	theme = "github_dark",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  hl_override = {
+    NvimTreeGitDirty = { fg = "#75cb78" },
+    NvimTreeOpenedFolderName = { fg = "#c7c7c7" },
+    NvimTreeFolderName = { fg = "#c7c7c7" },
+  },
 }
-
-
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
---}
 
 return M
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -4,3 +4,5 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+--
+vim.opt.relativenumber = true
